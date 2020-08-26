cf943225a1c5a33a60c5cccf5b9b4ed840002be1
Soumen Samanta <soumensamanta39@gmail.com>
2020-08-26 12:31:54 +0530

    Second Commit

--- oar.org.Core Java/src/Exceptions/Demo3.java
+++ oar.org.Core Java/src/Exceptions/Demo3.java
@@ -1,21 +1,28 @@
+/*Exception Propagation:-
+		Inside a method, if an exception raised and if we are not handled that exception, then the exception will be propagated to the called method.
+  Then caller method will be the responsible to handle the exception. This is known as Exception Propagation. */
+
+
 package com.oecm3.Exceptions;
 
 public class Demo3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("main starts");
 		m1();
 		System.out.println("main ends");
-		}
+	}
 	static void m1()
 	{
 		System.out.println("m1 starts");
-		//try
-		//{
+		try
+		{
 			m2();
-		//}catch(ArithmeticException e){
-			//System.out.println("exception caught in m1()");
-		//}
+		}catch(ArithmeticException e)
+		{
+			System.out.println("exception caught in m1()");
+		}
 		System.out.println("m1 ends");
 	}
 	static void m2()
@@ -32,7 +39,7 @@ public class Demo3 {
 	static void m3()
 	{
 		System.out.println("m3 starts");
-		int i=10/0;
+		int i=10/0; //here this exception will propagate to the caller method .i.e; m2() and m2() will handle the exception
 		System.out.println("m3 ends");
 	}
 }
